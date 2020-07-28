d19c6eedeaa211d64a6203582f8ecd3b0224d89c
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-07-28 19:53:33 +0500

    Fixed a bug

--- txprocessor/processor/main.py
+++ txprocessor/processor/main.py
@@ -1,12 +1,12 @@
 from sawtooth_sdk.processor.core import TransactionProcessor
-from txprocessor.processor.handler import BlockONSTXHandler
+from handler import BlockONSTXHandler
 
 def main():
     # In docker, the url would be the validator's container name with
     # port 4004
     processor = TransactionProcessor(url='tcp://127.0.0.1:4004')
 
-    handler = BlockONSTXHandler()
+    handler = BlockONSTXHandler("007007")
 
     processor.add_handler(handler)
 
